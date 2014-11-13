eab8c18f8f976e2e11d37dfeb906ffe2904c4b5f
Ahmed Issa <neuissa@gmail.com>
2014-11-14 00:44:06 +0100

    fix a bug

--- app/scripts/services/users.js
+++ app/scripts/services/users.js
@@ -8,11 +8,9 @@
  * Controller of the alumniWebApp
  */
 angular.module('alumniWebApp')
-  .factory('usersFactory', ['$http','API_HOST', function (API_HOST,$http) {
-        var urlBase = '//'+API_HOST+'/users';
-
+  .factory('usersFactory', ['$http','API_HOST', function ($http, API_HOST) {
+        var urlBase = '//' + API_HOST + '/users';
         var usersFactory = {};
-
 
         usersFactory.getUsers = function  () {
             return $http.get(urlBase);
@@ -21,8 +19,6 @@ angular.module('alumniWebApp')
         usersFactory.insertUser = function  (user) {
             return $http.post(urlBase, {user: user});
         };
-
-
 
         return usersFactory;
   }]);
