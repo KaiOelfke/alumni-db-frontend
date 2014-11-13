8cb6601dccc37ae13f5dc9c4929bc37e1c863d3d
Ahmed Issa <neuissa@gmail.com>
2014-11-13 23:36:04 +0100

    switch back to master after deploy and remove deploy branch

--- deploy.sh
+++ deploy.sh
@@ -4,15 +4,22 @@
 
   git remote add web-client-heroku git@heroku.com:alumni-db-frontend.git
 
-  # Remove tmp-deploy
-  git branch -D tmp-deploy
   # Create a temp branch to deploy from.
   git checkout -b tmp-deploy
+
   # Un-ignoring dist for a second.
   sed '/dist/d' .gitignore > .gitignore.new && mv .gitignore.new .gitignore
+
   # generate dist folder
   grunt
   
+  # push to heroku
   git add --all && git commit -a -m 'Deploy message.'
   
   yes | git push heroku `git subtree split --prefix dist tmp-deploy`:master --force
+
+  # Remove tmp-deploy
+  git branch -D tmp-deploy
+
+  # Switch back to master
+  git checkout master
