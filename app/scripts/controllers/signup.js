ab08123284deff43112f5cffce65fd058b9651ee
Can Göktas <can.goektas@gmail.com>
2014-12-27 19:19:05 +0100

    Move signupForm initialization into controller

--- app/scripts/controllers/signup.js
+++ app/scripts/controllers/signup.js
@@ -9,27 +9,27 @@
  * Controller of the alumni-db-frontend
  */
 
-function signupCtrl($auth, $state, $scope) {
-  $scope.handleSignUpBtnClick = function() {
-    $auth.submitRegistration($scope.signupForm)
-      .then(function(resp) {
-        $state.go('home.registration');
-        console.log('You have successfully signed up. ', resp);
-      })
-      .catch(function(resp) {
-        $scope.signupForm.alerts = {type: 'error', msg: ''};      
-        console.log('your information is wrong. ', resp);
-      });
-  };
-}
-
 var app = angular.module('alumni-db-frontend');
 
 app.controller('SignupCtrl', [
   '$auth', 
   '$state', 
   '$scope', 
-  signupCtrl
+  function($auth, $state, $scope) {
+    $scope.signupForm = {};
+
+    $scope.handleSignUpBtnClick = function() {
+      $auth.submitRegistration($scope.signupForm)
+        .then(function(resp) {
+          $state.go('home.registration');
+          console.log('You have successfully signed up. ', resp);
+        })
+        .catch(function(resp) {
+          $scope.signupForm.alerts = {type: 'error', msg: ''};      
+          console.log('Something went wrong. ', resp);
+        });
+    };
+  }
 ]);
 
 })();
