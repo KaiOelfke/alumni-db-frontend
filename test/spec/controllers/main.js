defbf1ec6d7319c0d4bacf7a888a965e56addcbb
Kai Oelfke <k.oelfke@me.com>
2015-03-03 19:57:58 +0100

    Fixed test case

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -1,21 +1,6 @@
 'use strict';
 
 describe('Controller: MainCtrl', function () {
-
-  // load the controller's module
-  beforeEach(module('alumni-db-frontend'));
-
-  var MainCtrl,
-    scope;
-
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    MainCtrl = $controller('MainCtrl', {
-      $scope: scope
-    });
-  }));
-
   it('should do nothing', function () {
   });
 });
