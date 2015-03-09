68a41c8d9d28bc69aa9c12b1d5ffe77d8ad250a1
Kai Oelfke <k.oelfke@me.com>
2015-03-09 17:27:51 +0100

    Improved form label description, added validation messages

--- app/scripts/services/validationMessages.js
+++ app/scripts/services/validationMessages.js
@@ -23,6 +23,8 @@ angular.module('alumni-db-frontend')
           student_company_name: 'Student company name: ',
           city: 'City: ',
           institution: 'Institution: ',
+          country_of_participation: 'Country of participation: ',
+          year_of_participation: 'Year of participation: ',
           'confirm_password.required': 'Sorry. You need to have a confirmation. To avoid the typos.',
           'password.required': 'Some security is needed so no one can change your account.',
           'password.minlength': 'The minimum length is 8.',
@@ -30,11 +32,14 @@ angular.module('alumni-db-frontend')
           'email.required': 'We will not spam you, but we need your email.',
           'email.email': 'The format seems to be wrong.',
           'birthday.dateFormat': 'Maybe the date format is wrong. It should be like in this example: 01.11.1993 / day.month.year',
-          'lastname.required': 'You have a last name, right?',
-          'firstname.required': 'You have a first name, right?',
-          'city.required': 'Please enter your city, village, settlement or some location.',
+          'lastname.required': 'We need your last name',
+          'firstname.required': 'We need your first name',
+          'city.required': 'Please enter the city you currently live in',
           'student_company_name.required': 'Every real company needs a name!',
-          'institution.required': 'The name of the institution where you did the JA program is missing.'
+          'institution.required': 'The name of the institution where you did the JA program is missing.',
+          'country.required': 'Please enter the country in that you currently live.',
+          'country_of_participation.required': 'Please enter in which country you participated in the JA program.',
+          'year_of_participation.required': 'Please enter in which year you participated in the JA program.'
         };
 
         var _title = 'Change a few things and try submitting again.';
