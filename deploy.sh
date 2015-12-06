fc27e36a97b650ac561c9ce239adaa8771adc57a
ahmedissa <neuissa@gmail.com>
2015-12-06 23:42:34 +0100

    deploy file

--- deploy.sh
+++ deploy.sh
@@ -30,17 +30,23 @@
   then 
     create_tmp_branch
     yes | git push heroku-dev `git subtree split --prefix dist tmp-deploy`:master --force
+    # Switch back to master
+    git checkout development
+    # Remove tmp-deploy
+    git branch -D tmp-deploy    
+
   elif [ $branch_name  -eq "master" ]
   then
     create_tmp_branch
     yes | git push heroku-prod `git subtree split --prefix dist tmp-deploy`:master --force
+    # Switch back to master
+    git checkout master
+    # Remove tmp-deploy
+    git branch -D tmp-deploy    
+    
   else 
     echo 'wrong arguments'
   fi 
 
 
-  # Switch back to master
-  git checkout master
 
-  # Remove tmp-deploy
-  git branch -D tmp-deploy
