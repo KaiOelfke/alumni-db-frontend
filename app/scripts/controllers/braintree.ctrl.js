d1e3f5285050af988386f4689aea1e0e8495a854
Can Göktas <can.goektas@gmail.com>
2016-03-10 14:16:51 +0100

    Hide pay button when processing payment

--- app/scripts/controllers/braintree.ctrl.js
+++ app/scripts/controllers/braintree.ctrl.js
@@ -12,7 +12,47 @@ angular
   .module('alumni-db-frontend')
   .controller('braintreeCtrl', ['braintreeService', 'plansFactory', '$state',  '$rootScope', '$scope', function(braintreeService, plansFactory, $state, $rootScope, $scope) {
 
+    $scope.title = 'Become a premium member!';
     $scope.braintreeReady = false;
+    $scope.processingPayment = false;
+
+    plansFactory
+      .getAllPlans()
+      .then(function(plansResponse) {
+        $scope.plans = plansResponse.data;
+        $scope.defaultPlan = getDefaultPlan($scope.plans);
+      }, function() {
+
+        console.error('Could not get plans');
+        window.alert('Ups, there was an error! Please refresh the website or contact a developer for assistance.');
+      });
+
+    braintreeService
+      .getClientToken()
+      .then(function(clientToken) {
+        // Set up braintree dropin
+        braintree.setup(clientToken, 'dropin', {
+          container: 'payment-form',
+          onPaymentMethodReceived: finishPaymentProcess,
+
+          onReady: function(integration) {
+            $scope.$apply(function() {
+              $scope.braintreeReady = true;
+            });
+          },
+
+          onError: function(error) {
+            console.log('an error occurred');
+            console.error(error);
+            finishPaymentProcess();
+          },
+        });
+      }, function() {
+
+        console.error('Could not get client token');
+        window.alert('Ups, there was an error! Please refresh the website or contact a developer for assistance.');
+      });
+
     var getDefaultPlan = function(plans) {
       for (var i = 0; i < plans.length; i++) {
         var plan = plans[i];
@@ -22,47 +62,37 @@ angular
       }
     };
 
-    plansFactory
-      .getAllPlans()
-      .then(function(plansResponse) {
-        $scope.plans = plansResponse.data;
-        $scope.defaultPlan = getDefaultPlan($scope.plans);
-      });
+    $scope.processPayment = function() {
+      $scope.processingPayment = true;
+      return true;
+    };
 
-    braintreeService
-      .getClientToken()
-      .then(function(clientToken) {
-        // Set up braintree dropin
-        braintree.setup(clientToken, 'dropin', {
-          container: 'payment-form',
-          onPaymentMethodReceived: function(paymentData) {
-            paymentData.userId = $rootScope.user.id;
-            braintreeService
-              .submitPayment(paymentData)
-              .then(function(response) {
-                console.log('Submitted payment successfully');
+    var finishPaymentProcess = function(paymentData) {
+      if (paymentData) {
+        paymentData.userId = $rootScope.user.id;
+        braintreeService
+          .submitPayment(paymentData)
+          .then(function(response) {
+            console.log('Submitted payment successfully');
 
-                // should be wrapped with somewhere else
-                var updatedUser = response.data.data;
-                angular.extend($rootScope.user, updatedUser);
-                $rootScope.user.statuses.push('premium');
+            // should be wrapped with somewhere else
+            var updatedUser = response.data.data;
+            angular.extend($rootScope.user, updatedUser);
+            $rootScope.user.statuses.push('premium');
 
-                $state.go('home.premium');
-              });
-          },
+            $state.go('home.premium');
 
-          onReady: function(integration) {
-            $scope.$apply(function() {
-              $scope.braintreeReady = true;
-            });
-          },
+            $scope.processingPayment = false;
+          }, function() {
 
-          onError: function(error) {
-            console.error(error);
-          },
-        });
-      });
-
-    $scope.title = 'Become a premium member!';
+            console.error('Something went wrong with braintreeService.submitPayment');
+            $scope.processingPayment = false;
+          });
+      } else {
+        console.log('Setting processing flag to false');
+        $scope.processingPayment = false;
+        $scope.$digest();
+      }
+    };
 
   }]);
