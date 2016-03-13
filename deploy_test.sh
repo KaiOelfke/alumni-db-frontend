42f4b7b56a96bed1e1928ee86184f07a38541163
ahmedissa <neuissa@gmail.com>
2016-03-13 23:41:13 +0100

    update deploy script

---
+++ deploy_test.sh
@@ -0,0 +1,19 @@
+    
+cd $CI_HOME
+
+branch_name=$(git rev-parse --abbrev-ref HEAD)
+
+if [ $branch_name = "development" ]
+then 
+  grunt build-staging
+  cd dist
+elif [ $branch_name  = "master" ]
+then
+  grunt build-production
+  cd dist
+else 
+  echo 'nothing'
+fi 
+
+
+
