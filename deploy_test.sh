07d6387cfb07610b1faff1db5325394b7ef398fc
ahmedissa <neuissa@gmail.com>
2016-03-14 01:42:39 +0100

    dump commit

--- deploy_test.sh
+++ deploy_test.sh
@@ -5,11 +5,9 @@ echo $TRAVIS_BRANCH
 if [ $TRAVIS_BRANCH = "development" ]
 then 
   grunt build-staging
-  cd dist
 elif [ $TRAVIS_BRANCH  = "master" ]
 then
   grunt build-production
-  cd dist
 else 
   echo 'nothing'
 fi 
