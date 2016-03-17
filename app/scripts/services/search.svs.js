1603ede42edbc96c1df13b65612343d88f162a92
Can Göktas <can.goektas@gmail.com>
2016-03-17 20:03:03 +0100

    Add search service

---
+++ app/scripts/services/search.svs.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name searchService
+ *
+ * @description A service which provides different function to interact
+ * with the backend search API.
+ */
+angular
+  .module('alumni-db-frontend')
+  .service('searchService', ['$http', 'API_HOST', function($http, API_HOST) {
+    const urlBase = API_HOST + '/search';
+    var searchService = {};
+
+    /**
+     * Executes a simple search for a user by a given text without any filters.
+     * @param  {string} searchText The search text to use.
+     * @return {Object}            A promise object returned bei the $http
+     *                             service.
+     */
+    searchService.userSearch = function(searchText) {
+      return $http.get(urlBase, {
+        params: {
+          text: searchText
+        }
+      });
+    };
+
+    return searchService;
+  }]);
