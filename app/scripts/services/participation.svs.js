b52ab4cc822f476226ae86d064280be30a0a92c4
Can Göktas <can.goektas@gmail.com>
2016-05-07 21:26:34 +0200

    Continue adding mocked participation functions
    - getParticipationsForEvent created
    - removeParticipation created

--- app/scripts/services/participation.svs.js
+++ app/scripts/services/participation.svs.js
@@ -18,8 +18,8 @@ angular
       var participationService = {};
 
       participationService.createParticipation = function(event_id, user_id) {
-        var eventKey = event_id + "";
         var deferred = $q.defer();
+        var eventKey = event_id + "";
         if (!participations[eventKey]) {
           participations[eventKey] = [user_id];
           deferred.resolve();
@@ -28,6 +28,30 @@ angular
             participations[eventKey].push(user_id)
           } else {
             deferred.reject();
+          }
+        }
+        return deferred.promise;
+      };
+
+      participationService.getParticipationsForEvent = function(event_id) {
+        var deferred = $q.defer();
+        var eventKey = event_id + "";
+        if (participations.hasOwnProperty(eventKey)) {
+          deferred.resolve(participations[eventKey])
+        } else {
+          deferred.reject();
+        }
+        return deferred.promise;
+      };
+
+      participationService.removeParticipation = function(event_id) {
+        var deferred = $q.defer();
+        var eventKey = event_id + "";
+        if (participations.hasOwnProperty(eventKey)) {
+          participations[eventKey] = null;
+          deferred.resolve();
+        } else {
+          deferred.reject();
         }
         return deferred.promise;
       };
