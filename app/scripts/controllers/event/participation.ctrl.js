022579abb1032ccee6e0025631a9617c02679ebb
Can Göktas <can.goektas@gmail.com>
2016-05-21 18:50:21 +0200

    Get event information in participation view

--- app/scripts/controllers/event/participation.ctrl.js
+++ app/scripts/controllers/event/participation.ctrl.js
@@ -5,8 +5,24 @@ angular
   .controller('participationCtrl', [
     '$scope',
     '$state',
-    '$stateParams', function($scope, $state, $stateParams) {
+    '$stateParams',
+    'eventService',
+    'displayErrors', function($scope, $state, $stateParams, eventService, displayErrors) {
 
       $scope.eventId = $stateParams.id;
+
+      $scope.event;
+
+      eventService
+        .getEvent($scope.eventId)
+        .then(
+          function successCallback(event) {
+            $scope.event = event;
+          },
+          // TODO: use toaster for displaying this error message
+          function errorCallback(response) {
+            console.error(displayErrors.convertErrorResponse(response));
+          }
+        );
 
     }]);
