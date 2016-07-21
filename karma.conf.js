22b4cadbaffc60c60b7d6975eef262822873166a
Kai Oelfke <k.oelfke@me.com>
2016-07-21 15:51:19 +0200

    Added angular file saver also to karma files

--- karma.conf.js
+++ karma.conf.js
@@ -21,6 +21,7 @@ module.exports = function(config) {
       'bower_components/angularjs-toaster/toaster.js',
       'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
       'bower_components/ng-file-upload/angular-file-upload.js',
+      'bower_components/angular-file-saver/dist/angular-file-saver.bundle.js',
       'app/scripts/**/*.js',
       'https://js.braintreegateway.com/v2/braintree.js',
       'test/mock/**/*.js',
