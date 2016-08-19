ca3207cb4f246f8eba18165ae2bde839d1767900
ahmedissa <neuissa@gmail.com>
2016-08-19 03:19:42 +0200

    begin with event participation

--- client/app/components/events/event/event.controller.js
+++ client/app/components/events/event/event.controller.js
@@ -15,6 +15,7 @@ class eventController {
     } else {
       this.event = this.eventFees;
     }
+    this.cards = ['event'];
     this.eventTypes = this.events.eventTypes;
     this.event.logo_photo.url  = this.changeStartOfAvatarUrl(this.event.logo_photo.url);
     this.event.cover_photo.url  = this.changeStartOfAvatarUrl(this.event.cover_photo.url);
@@ -26,6 +27,45 @@ class eventController {
       return 'http://localhost:3000' + url;
     }     
     return url; 
+  }
+
+  attend() {
+    switch(this.event.etype) {
+      case 'without_application_payment':
+        // attend participation api
+
+        break;
+
+      case 'with_payment':
+        // show payment and code
+        // then checkout participation api
+        this.cards = ['fees', 'discount']; 
+
+        break;
+
+      case 'with_application':
+        // if applied show code
+        // if not show application 
+        // application or code
+        this.cards = ['application']; 
+
+        break;
+
+      case 'with_payment_application':
+        // if applied show code
+        // if not show application       
+        // application then fee and code
+        // then checkout participation api
+
+        this.cards = ['application']; 
+
+
+        break; 
+
+    }
+  }
+
+  showCard() {
   }
 
 
