63e75446de7ba8da7398e801e17b8c080ccf9a9b
ahmedissa <neuissa@gmail.com>
2016-08-22 14:57:40 +0200

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 if [ $TRAVIS_BRANCH = "development" ]
 then 
-  NODE_ENV=staging
-  gulp build
+  NODE_ENV=staging gulp build
+  
   echo $NODE_ENV
 elif [ $TRAVIS_BRANCH  = "master" ]
 then
-  NODE_ENV=production
-  gulp build
+  NODE_ENV=production gulp build
+  
 else 
   echo 'nothing'
 fi 
